73ea7d4aa861ee0bad53ceaa697f60b1c88323f4
Hurtony Péter <hurtony.peter@encosoft.hu>
2019-01-24 13:41:01 +0100

    multi injector demo

--- src/app/books/pages/book-edit.page.component.ts
+++ src/app/books/pages/book-edit.page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { BookDetailsModel } from '../models/book-details.model';
 import { BooksApiClientService } from '../clients/books-api-client.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { BookFormModel } from '../components/book-form/book-form.model';
+import { HTTP_INTERCEPTORS, HttpInterceptor } from '@angular/common/http';
 
 @Component({
   templateUrl: './book-edit.page.component.html'
@@ -15,8 +16,11 @@ export class BookEditPageComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private booksApiClient: BooksApiClientService
-  ) { }
+    private booksApiClient: BooksApiClientService,
+    @Inject(HTTP_INTERCEPTORS) validators
+  ) {
+    console.log(validators);
+  }
 
   ngOnInit() {
     const id = +this.route.snapshot.paramMap.get('id');
